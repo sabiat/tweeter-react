2cabf4e5e8586f3ec1b8c5c6e0bb14f29604039d
Sabia Tahir <sabiatahir@hotmail.com>
2021-04-28 22:01:33 -0400

    Add profile and tweet form components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import { Navigation } from "./Navigation";
+
+const Profile = () => {
+  return (
+    <header class="main-header">
+      <div>
+        <img src="/images/profile-hex.png" />
+      </div>
+      <br />
+      <div>
+        <h2>Your Name</h2>
+      </div>
+    </header>
+  );
+};
+
+const TweetForm = () => {
+  return (
+    <section class="new-tweet">
+      <div class="error">
+        <span id="error-text"></span>
+      </div>
+      <form method="POST" action="/tweets/">
+        <label for="tweet-text">What are you humming about?</label>
+        <textarea name="text" id="tweet-text"></textarea>
+        <div class="button-counter">
+          <button type="submit">Tweet</button>
+          <output name="counter" class="counter" for="tweet-text">
+            140
+          </output>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+// const Tweet = () => {
+//   return ()
+// }
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
