eac7b5c27a4c0cf8490f7a94f931f7d440efcd97
Sabia Tahir <sabiatahir@hotmail.com>
2021-04-28 22:04:03 -0400

    Add tweet component

--- src/App.js
+++ src/App.js
@@ -35,9 +35,28 @@ const TweetForm = () => {
   );
 };
 
-// const Tweet = () => {
-//   return ()
-// }
+const Tweet = () => {
+  return (
+    <article class="tweet">
+      <header>
+        <div class="left-side">
+          <img src="https://i.imgur.com/nlhLi3I.png" alt="usericon" />
+          <span>Descartes</span>
+        </div>
+        <p class="hidden-username">@rd</p>
+      </header>
+      <div class="tweet-feed">Je pense , donc je suis</div>
+      <footer>
+        <p>a day ago</p>
+        <div class="icons">
+          <i class="fas fa-flag" aria-hidden="true"></i>
+          <i class="fas fa-retweet" aria-hidden="true"></i>
+          <i class="far fa-heart" aria-hidden="true"></i>
+        </div>
+      </footer>
+    </article>
+  );
+};
 
 function App() {
   return (
